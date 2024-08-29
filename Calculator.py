e11d5277ff30484962371bc341530472c62068d5
Dharshika-11 <dharshikaselvaraj2006@gmail.com>
2024-08-29 11:53:12 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+a=int(input("enter the number: "))
+b=input("enter an oprator: ")
+c=int(input("enter the number: "))
+
+if b=="+":
+    print("the addition of",a, "and",c, "is",a+c)
+if b=="-":
+    print("the subraction of",c, "from",a, "is",a-c)
+if b=="*":
+    print("the multiplication of",a, "and",c, "is",a*c)
+if b=="/":
+    print("the division of",a, "by",c, "is",a/c)  
